d067d4acb49b6492bd08f671b27212f7f860f589
P.R. Neider-Ball <phillrballn@icloud.com>
2025-06-11 09:13:42 -0500

    new feature
    added random verse feature

--- public/script.js
+++ public/script.js
@@ -1,5 +1,22 @@
 const btn = document.querySelector(".verse__btn");
+const verseRef = document.querySelector(".verse__ref");
+const verseText = document.querySelector(".verse__text");
 
-btn.addEventListener("click", () => {
-    console.log("Button clicked");
-});
+async function fetchVerse() {
+  verseText.textContent = "Loading, one moment...";
+  try {
+    const res = await fetch("/api/verse");
+    const data = await res.json();
+
+    verseText.textContent = data.text.trim();
+    verseRef.textContent = `— ${data.reference}`;
+  } catch (err) {
+    console.error("Fetch error:", err);
+    verseText.textContent = "Something went wrong. Try again later.";
+    verseRef.textContent = "";
+  }
+}
+
+fetchVerse()
+
+btn.addEventListener("click", fetchVerse);
